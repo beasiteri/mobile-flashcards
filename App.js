301bc5292f266402dd626f7092dc3462c5693e05
bea <siteribea@gmail.com>
2021-03-09 22:17:12 +0100

    Add provider to app component

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
-import * as React from "react";
+import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <Text>Cards</Text>
+        </View>
+      </Provider>
+    );
+  }
 }
