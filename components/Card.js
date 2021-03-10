11c0d0c3ca236328fbc50990dba69f0421396253
bea <siteribea@gmail.com>
2021-03-10 16:49:20 +0100

    Create single Card component

---
+++ components/Card.js
@@ -0,0 +1,14 @@
+import React, {Component } from "react";
+import { View, Text } from "react-native";
+
+class Card extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Single card component</Text>
+            </View>
+        );
+    }
+}
+
+export default Card;
